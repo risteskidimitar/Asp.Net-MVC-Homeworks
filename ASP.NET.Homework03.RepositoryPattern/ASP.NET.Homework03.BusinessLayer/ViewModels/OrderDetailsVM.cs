dc02005e2436e43e927b363a4e7e37174ef8d97c
risteskidimitar <60626102+risteskidimitar@users.noreply.github.com>
2020-09-15 20:59:52 +0200

    update

--- ASP.NET.Homework03.RepositoryPattern/ASP.NET.Homework03.BusinessLayer/ViewModels/OrderDetailsVM.cs
+++ ASP.NET.Homework03.RepositoryPattern/ASP.NET.Homework03.BusinessLayer/ViewModels/OrderDetailsVM.cs
@@ -21,7 +21,6 @@ namespace ASP.NET.Homework03.BusinessLayer.ViewModels
 
         [Required]
         //[Range(1, 100, ErrorMessage = "Range must be betwen 1 and 100 - custom")]
-        //neshto novo
         public long Phone { get; set; }
     }
 }
